561e1994cb790a5c7affdd87ee9fdb8eca1898a9
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-11-10 17:08:08 +0900

    Update Discord.lua

--- VortexCompany/C4/Discord.lua
+++ VortexCompany/C4/Discord.lua
@@ -21,7 +21,7 @@ _G.Settings = {
 
 local COREGUI = game:GetService("CoreGui")
 -- (62x) - final: 100 (JK)
-local version = "2.3.1" -- reverted version
+local version = "2.3.2" -- reverted version
 
 -- _G.Settings.banwaves
 -- _G.Settings.FreezeFling
@@ -9508,14 +9508,20 @@ if game.PlaceId == 1240123653 or game.PlaceId == 1632210982 then
     end
 end)
 end
-if cmd == "dance" then
+if cmd == "dance" then -- shitty command
 local dances = {"27789359","30196114","248263260","45834924","33796059","28488254","52155728"}
 Vortex:CheckError(function()
 	if r15(speaker) then
 		dances = {"3333432454","4555808220","4049037604","4555782893","10214311282","10714010337","10713981723","10714372526","10714076981","10714392151","11444443576"}
 		--dances = {3333432454,4555808220,4049037604,4555782893,10214311282,10714010337,10713981723,10714372526,10714076981,10714392151,11444443576}
 		--speaker.Character.Humanoid:PlayEmoteAndGetAnimTrackById(dances[math.random(1,#dances)])
-		speaker.Character.Humanoid:PlayEmote("rbxassetid://" .. dances[math.random(1,#dances)])
+		local emoteAsset = MarketplaceService:GetProductInfo(dances[math.random(1,#dances)])
+                if emoteAsset and emoteAsset.AssetTypeId == Enum.AssetType.Emote then
+                   speaker.Character.Humanoid:PlayEmote(emoteAsset.AssetId)
+                else
+                   Vortex:PromptUI("Invalid ID or not an emote ID.")
+                end
+		--speaker.Character.Humanoid:PlayEmote("rbxassetid://" .. dances[math.random(1,#dances)])
 	else
 		local animation = Instance.new("Animation")
 	        animation.AnimationId = "rbxassetid://" .. dances[math.random(1, #dances)]
