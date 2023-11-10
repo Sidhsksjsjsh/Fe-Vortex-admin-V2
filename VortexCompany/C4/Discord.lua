be254b30d86dedbb9009d7ec216c687e296c3430
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-11-10 16:37:22 +0900

    Update Discord.lua

--- VortexCompany/C4/Discord.lua
+++ VortexCompany/C4/Discord.lua
@@ -21,7 +21,7 @@ _G.Settings = {
 
 local COREGUI = game:GetService("CoreGui")
 -- (62x) - final: 100 (JK)
-local version = "2.2.9" -- reverted version
+local version = "2.3.0" -- reverted version
 
 -- _G.Settings.banwaves
 -- _G.Settings.FreezeFling
@@ -9514,7 +9514,8 @@ Vortex:CheckError(function()
 	if r15(speaker) then
 		--dances = {"3333432454","4555808220","4049037604","4555782893","10214311282","10714010337","10713981723","10714372526","10714076981","10714392151","11444443576"}
 		dances = {3333432454,4555808220,4049037604,4555782893,10214311282,10714010337,10713981723,10714372526,10714076981,10714392151,11444443576}
-		speaker.Character.Humanoid:PlayEmoteAndGetAnimTrackById(dances[math.random(1,#dances)])
+		--speaker.Character.Humanoid:PlayEmoteAndGetAnimTrackById(dances[math.random(1,#dances)])
+		speaker.Character.Humanoid:PlayEmote(dances[math.random(1,#dances)])
 	else
 		local animation = Instance.new("Animation")
 	        animation.AnimationId = "rbxassetid://" .. dances[math.random(1, #dances)]
