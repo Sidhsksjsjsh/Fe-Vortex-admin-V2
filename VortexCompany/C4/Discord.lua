727b9cb29593bc3664f465ac13983e69881da09b
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-02-17 14:12:53 +0900

    Update Discord.lua

--- VortexCompany/C4/Discord.lua
+++ VortexCompany/C4/Discord.lua
@@ -21,7 +21,7 @@ _G.Settings = {
 
 local COREGUI = game:GetService("CoreGui")
 -- (62x) - final: 100 JSKV5
-local version = "-MAX | VortexOS V6.0.2" -- reverted version
+local version = "-MAX | VortexOS V6.0.3" -- reverted version
 
 -- _G.Settings.banwaves
 -- _G.Settings.FreezeFling
@@ -453,7 +453,7 @@ local UrlScript = {
 	"https://raw.githubusercontent.com/Turtle-Secure/Anime-Dungeon-Fighters/main/.lua",
 	"https://raw.githubusercontent.com/Turtle-Secure/Gun-Ball/main/.lua",
 	"https://raw.githubusercontent.com/Turtle-Secure/Mowing-Simulator/main/.lua",
-	"https://raw.githubusercontent.com/Turtle-Secure/Load-all-script-in-one-script/main/.lua"
+	"https://raw.githubusercontent.com/Turtle-Secure/Anime-Infinity/main/.lua"
 }
 
 function dragify(Frame)
@@ -9678,7 +9678,7 @@ Vortex:CheckError(function()
         executeHTTPS(UrlScript[32])
 end)
 end
-if cmd == "lsc" or cmd == "loadsc" then
+if cmd == "fai" or cmd == "animeinf" then
 Vortex:CheckError(function()
 	notify("Injecting...","pls wait..")
         executeHTTPS(UrlScript[33])
@@ -10042,7 +10042,7 @@ cmds[#cmds + 1] = {Text = "[340] " .. tostring(prefix) .. "sbs / shootbeam",Titl
 cmds[#cmds + 1] = {Text = "[341] " .. tostring(prefix) .. "adf",Title = "Run Anime Dungeon Fighters Script!"}
 cmds[#cmds + 1] = {Text = "[342] " .. tostring(prefix) .. "gb / gunball",Title = "Run Gun Ball Script!"}
 cmds[#cmds + 1] = {Text = "[342] " .. tostring(prefix) .. "ms / mowingsim",Title = "Run Mowing Simulator Script!"}
-cmds[#cmds + 1] = {Text = "[343] " .. tostring(prefix) .. "lsc / loadsc",Title = "Run games script by using this command."}
+cmds[#cmds + 1] = {Text = "[343] " .. tostring(prefix) .. "fai / animeinf",Title = "Run Free Anime Infinity Script"}
 
 _G.RemoveSymbols = {
    blank = ""
